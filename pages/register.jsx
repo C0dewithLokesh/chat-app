bd85cde8c376e83a404333872002297ffd562d35
Lokesh <lokeshkaushik2243@gmail.com>
2023-09-03 17:31:20 +0530

    register page done

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 import { IoLogoGoogle, IoLogoFacebook } from "react-icons/io";
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="h-[100vh] flex justify-center items-center bg-c1">
-      <div className="flex items-center flex-col w-[600px]">
+      <div className="flex items-center flex-col">
         <div className="text-center">
-          <div className="text-4xl font-bold">Login to Your Account</div>
+          <div className="text-4xl font-bold">Create New Account</div>
           <div className="mt-3 text-c3">
             Connect and chat with anyone, anywhere
           </div>
@@ -33,9 +34,36 @@ const Login = () => {
           <span className="text-c3 font-semibold">OR</span>
           <span className="w-5 h-[1px] bg-c3"></span>
         </div>
+
+        <form action="" className="flex flex-col items-center gap-3 w-[500px] mt-5">
+          <input type="text" placeholder="Display Name"
+            className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
+            autoComplete="off"
+          />
+          <input type="email" placeholder="Email"
+            className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
+            autoComplete="off"
+          />
+          <input type="password" placeholder="Password"
+            className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
+            autoComplete="off"
+          />
+          <button className="mt-4 w-full h-14 rounded-xl outline-none text-base font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            Sign Up
+          </button>
+        </form>
+
+        <div className="flex justify-center gap-1 text-c3 mt-5">
+          <span>Already have an Account?</span>
+          <Link href="/login"
+            className="font-semibold text-white underline underline-offset-2 cursor-pointer"
+          >
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
