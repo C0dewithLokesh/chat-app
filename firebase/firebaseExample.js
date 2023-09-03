e3e90eddf5b7c756d281ced9e1629b56a4ac3342
Lokesh <lokeshkaushik2243@gmail.com>
2023-09-03 18:15:58 +0530

    firebase installation done

---
+++ firebase/firebaseExample.js
@@ -0,0 +1,18 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// import { getFirebase } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: ""
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const storage = getStorage(app);
+// export const db = getFirebase(app); 
